323e99a863183caa12d8b232ce2d4e52c3a03532
Virginia Chen <virginia.chen32@gmail.com>
2018-05-20 19:17:38 -0700

    Begin unit specs

--- expense_tracker/spec/spec_helper.rb
+++ expense_tracker/spec/spec_helper.rb
@@ -16,6 +16,7 @@ ENV['RACK_ENV'] = 'test'
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  config.filter_gems_from_backtrace 'rack', 'rack-test', 'sequel', 'sinatra'
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
@@ -48,7 +49,6 @@ RSpec.configure do |config|
 
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
-=begin
   # This allows you to limit a spec run to individual examples or groups
   # you care about by tagging them with `:focus` metadata. When nothing
   # is tagged with `:focus`, all examples get run. RSpec also provides
@@ -98,5 +98,4 @@ RSpec.configure do |config|
   # test failures related to randomization by passing the same `--seed` value
   # as the one that triggered the failure.
   Kernel.srand config.seed
-=end
 end
